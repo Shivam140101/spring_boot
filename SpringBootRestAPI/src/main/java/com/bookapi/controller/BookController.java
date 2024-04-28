591998fec37633658995810bad056308ea86101a
Shivam <shivamahlawat118@gmail.com>
2024-04-28 17:21:57 +0530

    Created Rest API using SpringBoot But not connected to database

---
+++ SpringBootRestAPI/src/main/java/com/bookapi/controller/BookController.java
@@ -0,0 +1,45 @@
+package com.bookapi.controller;
+
+
+import com.bookapi.entity.Book;
+import com.bookapi.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @Autowired
+    private BookService service;
+
+    @GetMapping("/books")
+    public List<Book> getBooks(){
+        return service.getAllBooks();
+    }
+
+    @GetMapping("/getBook/{id}")
+    public Book getBook(@PathVariable("id") int id){
+        return service.getBookById(id);
+    }
+
+    @PostMapping("/addBook")
+    public Book addBook(@RequestBody Book book){
+        return service.addBook(book);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteBook( @PathVariable("id") int id){
+        service.removeBook(id);
+        return "Book Deleted Successfully";
+    }
+
+    @PutMapping("update/{id}")
+    public String updateBook(@RequestBody Book book, @PathVariable("id") int id){
+        service.updateBook(book, id);
+        return "Book Updated Successfully";
+    }
+
+}
---
+++ SpringBootRestAPI/src/main/java/com/bookapi/service/BookService.java
@@ -0,0 +1,53 @@
+package com.bookapi.service;
+
+import com.bookapi.entity.Book;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class BookService {
+
+    private static List<Book> list = new ArrayList<Book>();
+
+    static {
+        list.add(new Book(12,"Python","Cody"));
+        list.add(new Book(13,"DBMS","Steve"));
+        list.add(new Book(14,"react","Shivam"));
+    }
+
+    // get All books
+    public List<Book> getAllBooks() {
+        return list;
+    }
+
+    // get Single Book By Id
+    public Book getBookById(int id) {
+        Book book;
+        book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+        return book;
+    }
+
+    public Book addBook(Book book) {
+        list.add(book);
+        return book;
+    }
+
+    public void removeBook(int id) {
+        list = list.stream().filter(e -> e.getId() != id).collect(Collectors.toList());
+    }
+
+    public void updateBook(Book book , int id){
+        list = list.stream().peek(b -> {
+            if (b.getId() == id){
+                b.setTitle(book.getTitle());
+                b.setAuthor(book.getAuthor());
+            }
+        }).collect(Collectors.toList());
+    }
+
+
+}
